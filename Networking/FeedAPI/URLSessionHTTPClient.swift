0d23e3a99f38bcc4f1686a25a7cf14260aba7681
SrilathaKarancheti29 <slkarancheti@gmail.com>
2022-05-08 12:08:54 -0400

    Updated the accessory in URLSessionHTTPClient class

--- Networking/FeedAPI/URLSessionHTTPClient.swift
+++ Networking/FeedAPI/URLSessionHTTPClient.swift
@@ -7,16 +7,16 @@
 
 import Foundation
 
-class URLSessionHTTPClient: HTTPClient {
+public class URLSessionHTTPClient: HTTPClient {
     private let session: URLSession
     
-    init(session: URLSession = .shared) {
+    public init(session: URLSession = .shared) {
         self.session = session
     }
     
-    struct UnexpectedValuesRepresentationError: Error {}
+    private struct UnexpectedValuesRepresentationError: Error {}
     
-    func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void) {
+    public func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void) {
         session.dataTask(with: url) { data, response, error in
             if let error = error {
                 completion(.failure(error))
